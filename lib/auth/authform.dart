e9c0affb83505e73db65d68de630754aa4d53978
AyushSantri <ayushsantri222@gmail.com>
2022-01-16 15:48:00 +0530

    added form for auth in authform.dartm

--- lib/auth/authform.dart
+++ lib/auth/authform.dart
@@ -8,8 +8,24 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: ListView(
+        children: [
+          Container(
+            child: Form(
+              key: _formkey,
+              child: Column(
+                children: [],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
