3f6ccbaad2f1ad66dd905e5559ca7b215fad80e3
AyushSantri <ayushsantri222@gmail.com>
2022-01-16 17:16:52 +0530

    designed authform.dart

--- lib/auth/authform.dart
+++ lib/auth/authform.dart
@@ -9,8 +9,11 @@ class AuthForm extends StatefulWidget {
 
 class _AuthFormState extends State<AuthForm> {
   final _formkey = GlobalKey<FormState>();
-  final _email = ' ';
-  final _password = ' ';
+  var _email = ' ';
+  var _password = ' ';
+  var _username = ' ';
+  bool isLoginPage = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,10 +22,94 @@ class _AuthFormState extends State<AuthForm> {
       child: ListView(
         children: [
           Container(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
             child: Form(
               key: _formkey,
               child: Column(
-                children: [],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (!isLoginPage)
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      key: const ValueKey('username'),
+                      validator: (value) {
+                        if (value?.isEmpty == true) {
+                          return 'Incorrect name';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _username = value!;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Enter Username",
+                        hintText: "John Doe",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide()),
+                      ),
+                    ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    key: const ValueKey('email'),
+                    validator: (value) {
+                      if (value?.isEmpty == true ||
+                          value?.contains('@') == false) {
+                        return 'Incorrect Email';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _email = value!;
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Enter email",
+                      hintText: "john123@gmail.com",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: const BorderSide()),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    key: const ValueKey('password'),
+                    validator: (value) {
+                      if (value?.isEmpty == true) {
+                        return 'password empty';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _password = value!;
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Enter password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: const BorderSide()),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(5),
+                      width: double.infinity,
+                      height: 70,
+                      child: RaisedButton(
+                          child: isLoginPage ? Text('Login') : Text('SignUp'),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          color: Theme.of(context).primaryColor,
+                          onPressed: () {}))
+                ],
               ),
             ),
           )
