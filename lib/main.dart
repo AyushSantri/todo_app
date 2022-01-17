590e0357ba592eec2204230495768b449900e582
AyushSantri <ayushsantri222@gmail.com>
2022-01-17 23:06:49 +0530

    added authentication through firebase and storing user data in firestore in main.dart and added firebase_core dependecy

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/auth/authscreen.dart';
 import 'package:todo_app/screens/home.dart';
@@ -12,7 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AuthScreen(),
+      home: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData)
+            return Home();
+          else
+            return AuthScreen();
+        },
+      ),
       debugShowCheckedModeBanner: false,
       theme:
           ThemeData(brightness: Brightness.dark, primaryColor: Colors.purple),
