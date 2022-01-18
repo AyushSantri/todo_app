40053a3ae339bc890b2728bdcde6c6c60d394020
AyushSantri <ayushsantri222@gmail.com>
2022-01-18 20:36:52 +0530

    gave UI to add task page

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AddTask extends StatefulWidget {
   const AddTask({Key? key}) : super(key: key);
@@ -12,7 +13,39 @@ class _AddTaskState extends State<AddTask> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('New Task'),
+        title: const Text('New Task'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                  border: const OutlineInputBorder(), labelText: 'Enter title'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Enter description'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Add task',
+                  style: GoogleFonts.roboto(fontSize: 18),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
